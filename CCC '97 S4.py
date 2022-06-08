702cc0bedac90cae0ca3d3fd2e808cb2f515c535
owen-wang-student <owang420@gmail.com>
2022-06-08 14:06:41 -0400

    completed CCC '97 S4'

--- CCC '97 S4.py
+++ CCC '97 S4.py
@@ -7,6 +7,7 @@ for j in range(numCases):
         string = ""
         line = list(input().split())
         if len(line) == 0:
+            if j != 0: print()
             break
         for i in line:
             if i not in censorWords:
@@ -17,17 +18,10 @@ for j in range(numCases):
                 string += str(censorWords.index(i)+1)
                 string += "P"
         output.append(string)
-    if j != 0:
-        print("")
     for k in output:
         pp = list(k.split("P"))
         pp.pop(len(pp)-1)
-        print(' '.join(pp))
+        print(" ".join(pp))
+print()
 
 
-# 2
-# the cat chased the rat while
-# the dog chased the cat into the rat house
-#
-# penis dog sucks cats penis
-# the dog dog cat bastard
