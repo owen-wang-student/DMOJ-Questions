7b4012cdd6c305761dbca537252dd22ca477df2c
owen-wang-student <owang420@gmail.com>
2022-06-29 07:31:00 -0400

    Uncompleted CCC '21 S2

---
+++ CCC '21 S2.py
@@ -0,0 +1,28 @@
+rows = int(input())
+columns = int(input())
+
+numMoves = int(input())
+
+r = c = 0
+
+row = []
+column = []
+
+for i in range(numMoves):
+    move = input().split()
+    if move[0] == "R":
+        if move[1] not in row:
+            row.append(move[1])
+            r += 1
+        else:
+            r -= 1
+    elif move[0] == "C":
+        if move[1] not in column:
+            column.append(move[1])
+            c += 1
+        else:
+            c -= 1
+
+print((columns*r) + (rows*c) - (2*r*c))
+
+#TLE LAST CASE :(((
---
+++ test.py
@@ -0,0 +1,3 @@
+l = [2, 1]
+l = l.sort()
+print(l)
